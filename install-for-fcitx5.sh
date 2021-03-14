108ed9004e0deba5b4e12019707a6a5a563a3c0e
beavailable <beavailable@protonmail.com>
2021-03-14 08:25:30 +0800

    init

---
+++ install-for-fcitx5.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+gen_formatted_dict() {
+    cat - better-wubi.dict <<EOF
+;fcitx 版本 0x03 码表文件
+键码=abcdefghijklmnopqrstuvwxy
+码长=4
+拼音=@
+拼音长度=4
+[组词规则]
+e2=p11+p12+p21+p22
+e3=p11+p21+p31+p32
+a4=p11+p21+p31+n11
+[数据]
+EOF
+}
+
+if [ $EUID != 0 ]; then
+    echo 'please run it with root'
+    exit 1
+fi
+if ! which libime_tabledict &>/dev/null; then
+    echo 'libime_tabledict not found'
+    exit 1
+fi
+dict=/usr/share/libime/better-wubi.main.dict
+conf_wbx=/usr/share/fcitx5/inputmethod/wbx.conf
+conf=/usr/share/fcitx5/inputmethod/better-wubi.conf
+
+libime_tabledict <(gen_formatted_dict) $dict
+
+pattern="s/^Name\[zh_CN]=五笔字型\$/Name[zh_CN]=新世纪五笔/;s?^File=.+\$?File=$dict?"
+sed -E "$pattern" $conf_wbx >$conf
---
+++ sort-dict.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat better-wubi.dict | sort -st ' ' -k 1,1 >better-wubi-sorted.dict
