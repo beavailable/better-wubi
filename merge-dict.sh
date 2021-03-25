9e1cea8ff20680fe09c578477dc41a2d5cebfd75
beavailable <beavailable@protonmail.com>
2021-03-25 17:53:32 +0800

    update merge-dict.sh

--- merge-dict.sh
+++ merge-dict.sh
@@ -4,5 +4,5 @@ if [ $# = 0 ]; then
     exit 1
 fi
 while read code chinese; do
-    sed -i -E "s/^.{${#code}}( $chinese)$/$code\1/" better-wubi.dict
+    sed -i -E "s/^.{${#code}} ($chinese)$/$code \1/" better-wubi.dict
 done <$1
