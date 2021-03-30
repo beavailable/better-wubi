5882689ee08771ed701d880dce3b47d08a60b6a9
beavailable <beavailable@protonmail.com>
2021-03-30 08:47:31 +0800

    update regenerate-dict.py

--- regenerate-dict.py
+++ regenerate-dict.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-char_lines, words = [], []
+result, words = [], []
 d = {}
 
 with open('better-wubi.dict') as f:
@@ -7,25 +7,28 @@ with open('better-wubi.dict') as f:
         line = f.readline()
         if not line:
             break
-        line = line[:-1]
-        code, value = line.split(' ')
+        idx = line.index(' ')
+        code, value = line[:idx], line[idx + 1:-1]
         if len(value) == 1:
-            char_lines.append(line)
+            result.append((code, value))
             if len(code) >= 2:
                 d[value] = code[:2]
         else:
             words.append(value)
 
+for word in words:
+    if len(word) == 2:
+        first, second = d[word[0]], d[word[1]]
+        result.append((f'{first}{second}', word))
+    elif len(word) == 3:
+        first, second, third = d[word[0]], d[word[1]], d[word[2]]
+        result.append((f'{first[0]}{second[0]}{third}', word))
+    else:
+        first, second, third, last = d[word[0]], d[word[1]], d[word[2]], d[word[-1]]
+        result.append((f'{first[0]}{second[0]}{third[0]}{last[0]}', word))
+
+result.sort(key=lambda it: it[0])
+
 with open('better-wubi.dict', 'w') as f:
-    for line in char_lines:
-        f.write(f'{line}\n')
-    for word in words:
-        if len(word) == 2:
-            first, second = d[word[0]], d[word[1]]
-            f.write(f'{first}{second} {word}\n')
-        elif len(word) == 3:
-            first, second, third = d[word[0]], d[word[1]], d[word[2]]
-            f.write(f'{first[0]}{second[0]}{third} {word}\n')
-        else:
-            first, second, third, last = d[word[0]], d[word[1]], d[word[2]], d[word[-1]]
-            f.write(f'{first[0]}{second[0]}{third[0]}{last[0]} {word}\n')
+    for code, value in result:
+        f.write(f'{code} {value}\n')
