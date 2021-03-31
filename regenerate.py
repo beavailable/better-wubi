63fc90daf13c98e73da20b9745c66c1f70baa6bc
beavailable <beavailable@protonmail.com>
2021-03-31 13:25:27 +0800

    update regenerate.py

--- regenerate.py
+++ regenerate.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 result, words = [], []
-d = {}
+cc = {}
 
 with open('better-wubi.dict') as f:
     while True:
@@ -12,19 +12,19 @@ with open('better-wubi.dict') as f:
         if len(value) == 1:
             result.append((code, value))
             if len(code) >= 2:
-                d[value] = code[:2]
+                cc[value] = code[:2]
         else:
             words.append(value)
 
 for word in words:
     if len(word) == 2:
-        first, second = d[word[0]], d[word[1]]
+        first, second = cc[word[0]], cc[word[1]]
         result.append((f'{first}{second}', word))
     elif len(word) == 3:
-        first, second, third = d[word[0]], d[word[1]], d[word[2]]
+        first, second, third = cc[word[0]], cc[word[1]], cc[word[2]]
         result.append((f'{first[0]}{second[0]}{third}', word))
     else:
-        first, second, third, last = d[word[0]], d[word[1]], d[word[2]], d[word[-1]]
+        first, second, third, last = cc[word[0]], cc[word[1]], cc[word[2]], cc[word[-1]]
         result.append((f'{first[0]}{second[0]}{third[0]}{last[0]}', word))
 
 result.sort(key=lambda it: it[0])
