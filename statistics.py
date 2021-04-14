4212e12c873658d4daeb36a9d3f94da51d9fdc00
beavailable <beavailable@protonmail.com>
2021-04-14 11:23:11 +0800

    update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,26 +1,26 @@
 #!/bin/python3
 
-def print_freq(freq, units):
+def print_freq(freq):
     left_total, right_total, total = 0, 0, 0
     for c in 'qwertasdfgxcvb':
         left_total += freq[c]
-        freq[c] = f'{freq[c]/units:.1f}'
     for c in 'yuiophjklnm':
         right_total += freq[c]
-        freq[c] = f'{freq[c]/units:.1f}'
     total = left_total + right_total
-    print('---------------------------------------------------------------------------------')
+    for c in 'abcdefghijklmnopqrstuvwxy':
+        freq[c] = f'{freq[c]/total:.1%}'
+    print('-------------------------------------------------------------------------------------------')
     print(f"| Q-{freq['q']} | W-{freq['w']} | E-{freq['e']} | R-{freq['r']} | T-{freq['t']} | Y-{freq['y']} | U-{freq['u']} | I-{freq['i']} | O-{freq['o']} | P-{freq['p']} |")
-    print('---------------------------------------------------------------------------------')
+    print('-------------------------------------------------------------------------------------------')
     print(f" | A-{freq['a']} | S-{freq['s']} | D-{freq['d']} | F-{freq['f']} | G-{freq['g']} | H-{freq['h']} | J-{freq['j']} | K-{freq['k']} | L-{freq['l']} |")
-    print(' -------------------------------------------------------------------------')
+    print(' ----------------------------------------------------------------------------------')
     print(
-        f"   | Z     | X-{freq['x']} | C-{freq['c']} | V-{freq['v']} | B-{freq['b']} | N-{freq['n']} | M-{freq['m']} |")
-    print('   ---------------------------------------------------------')
-    print(f'左边总计 {left_total}，右边总计 {right_total}，比例为 {left_total/total:.0%}:{right_total/total:.0%}')
+        f"   | Z      | X-{freq['x']} | C-{freq['c']} | V-{freq['v']} | B-{freq['b']} | N-{freq['n']} | M-{freq['m']} |")
+    print('   ----------------------------------------------------------------')
+    print(f'总计 {total}，左右比例为 {left_total/total:.1%}:{right_total/total:.1%}')
 
 
-def stat_list(lst, units):
+def stat_freq(lst):
     freq, prev = {}, ''
     for c in 'abcdefghijklmnopqrstuvwxy':
         freq[c] = 0
@@ -30,7 +30,32 @@ def stat_list(lst, units):
         for c in code:
             freq[c] += 1
         prev = value
-    print_freq(freq, units)
+    print_freq(freq)
+
+
+def stat_chongma(lst):
+    d = {}
+    for code, value in lst:
+        if len(code) <= 2:
+            continue
+        if code in d:
+            d[code] += 1
+        else:
+            d[code] = 1
+    code_total, value_total, codes, values = 0, 0, 0, 0
+    for code, count in d.items():
+        code_total += 1
+        value_total += count
+        if count > 1:
+            codes += 1
+            values += count
+    print('-----------------------------------------------')
+    print('| 指标 |    总计    |    重码    |    比例    |')
+    print('-----------------------------------------------')
+    print(f'| 编码 | {code_total:^10} | {codes:^10} | {codes/code_total:^10.1%} |')
+    print('-----------------------------------------------')
+    print(f'| 字词 | {value_total:^10} | {values:^10} | {values/value_total:^10.1%} |')
+    print('-----------------------------------------------')
 
 
 def main():
@@ -44,11 +69,19 @@ def main():
             else:
                 words.append((code, value))
     chars.sort(key=lambda it: it[1])
-    print('单字（以千为单位）：')
-    stat_list(chars, 1000)
+    print('单字频率：')
+    stat_freq(chars)
     print()
-    print('词组（以万为单位）：')
-    stat_list(words, 10000)
+    print('词组频率：')
+    stat_freq(words)
+    print()
+    print('单字重码：')
+    stat_chongma(chars)
+    print()
+    print('词组重码：')
+    stat_chongma(words)
+    print()
+    print('提示：词库不同、统计方法不同可能会导致结果不同，此数据仅供参考！')
 
 
 main()
