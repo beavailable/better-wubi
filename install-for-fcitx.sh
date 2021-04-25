c5480062e442a69bd05e959d2c92de932b9bced4
beavailable <beavailable@protonmail.com>
2021-04-26 07:33:34 +0800

    add fcitx support

---
+++ install-for-fcitx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $EUID != 0 ]; then
+    echo 'please run it with root permissions'
+    exit 1
+fi
+if ! which txt2mb &>/dev/null; then
+    echo 'txt2mb not found'
+    exit 1
+fi
+dict=better-wubi.mb
+conf_wbx=/usr/share/fcitx/table/wbx.conf
+conf=/usr/share/fcitx/table/better-wubi.conf
+
+txt2mb <(./convert.py fcitx) /usr/share/fcitx/table/$dict
+
+pattern="s/^(UniqueName=).+\$/\1better-wubi/;s/^(Name.*=).+\$/\1better-wubi/;s?^File=.+\$?File=$dict?"
+sed -E "$pattern" $conf_wbx >$conf
--- install-for-fcitx5.sh
+++ install-for-fcitx5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $EUID != 0 ]; then
-    echo 'please run it with root'
+    echo 'please run it with root permissions'
     exit 1
 fi
 if ! which libime_tabledict &>/dev/null; then
@@ -11,9 +11,9 @@ dict=/usr/share/libime/better-wubi.main.dict
 conf_wbx=/usr/share/fcitx5/inputmethod/wbx.conf
 conf=/usr/share/fcitx5/inputmethod/better-wubi.conf
 
-libime_tabledict <(./convert.py fcitx5) $dict
+libime_tabledict <(./convert.py fcitx) $dict
 
-pattern="s/^Name\[zh_CN]=五笔字型\$/Name[zh_CN]=better-wubi/;s?^File=.+\$?File=$dict?"
+pattern="s/^(Name.*=).+\$/\1better-wubi/;s?^File=.+\$?File=$dict?"
 sed -E "$pattern" $conf_wbx >$conf
 
 sudo -Eu $SUDO_USER bash <<EOF
