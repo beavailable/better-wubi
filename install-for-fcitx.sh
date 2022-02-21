aa81030f77eeae4a76149c86eb1856c2f2a60a36
beavailable <beavailable@protonmail.com>
2022-02-21 08:24:27 +0800

    Add an icon

--- install-for-fcitx.sh
+++ install-for-fcitx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 install_for_fcitx() {
-    local conf_wbx conf dict
+    local conf_wbx conf dict icon
     if ! which txt2mb &>/dev/null; then
         echo 'txt2mb not found'
         exit 1
@@ -8,16 +8,17 @@ install_for_fcitx() {
     conf_wbx=/usr/share/fcitx/table/wbx.conf
     conf=~/.config/fcitx/table/better-wubi.conf
     dict=better-wubi.mb
+    icon=~/.config/fcitx/table/better-wubi.png
 
     mkdir -p ~/.config/fcitx/table
     txt2mb <(./convert.py fcitx) ~/.config/fcitx/table/$dict
-
-    sed -E "s/^(UniqueName=).+\$/\1better-wubi/;s/^(Name.*=).+\$/\1better-wubi/;s?^File=.+\$?File=$dict?" $conf_wbx >$conf
+    cp ./icon.png $icon
+    sed -E -e "s/^(UniqueName=).+\$/\1better-wubi/" -e "s/^(Name.*=).+\$/\1better-wubi/" -e "s!^File=.+\$!File=$dict!" -e "s!^Icon=.+\$!Icon=$icon!" $conf_wbx >$conf
 
     dbus-send --type=method_call --dest=org.fcitx.Fcitx /inputmethod org.fcitx.Fcitx.InputMethod.Restart
 }
 install_for_fcitx5() {
-    local conf_wbx conf dict
+    local conf_wbx conf dict icon
     if ! which libime_tabledict &>/dev/null; then
         echo 'libime_tabledict not found'
         exit 1
@@ -25,11 +26,12 @@ install_for_fcitx5() {
     conf_wbx=/usr/share/fcitx5/inputmethod/wbx.conf
     conf=~/.local/share/fcitx5/inputmethod/better-wubi.conf
     dict=~/.local/share/fcitx5/inputmethod/better-wubi.main.dict
+    icon=~/.local/share/fcitx5/inputmethod/better-wubi.png
 
     mkdir -p ~/.local/share/fcitx5/inputmethod
     libime_tabledict <(./convert.py fcitx) $dict
-
-    sed -E "s/^(Name.*=).+\$/\1better-wubi/;s?^File=.+\$?File=$dict?" $conf_wbx >$conf
+    cp ./icon.png $icon
+    sed -E -e "s/^(Name.*=).+\$/\1better-wubi/" -e "s!^File=.+\$!File=$dict!" -e "s!^Icon=.+\$!Icon=$icon!" $conf_wbx >$conf
 
     fcitx5-remote -e && fcitx5-remote -c
 }
--- uninstall-for-fcitx.sh
+++ uninstall-for-fcitx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 uninstall_for_fcitx() {
-    rm -f ~/.config/fcitx/table/better-wubi.conf ~/.config/fcitx/table/better-wubi.mb
+    rm -f ~/.config/fcitx/table/better-wubi.{conf,mb,png}
 
     dbus-send --type=method_call --dest=org.fcitx.Fcitx /inputmethod org.fcitx.Fcitx.InputMethod.Restart
 }
 uninstall_for_fcitx5() {
-    rm -f ~/.local/share/fcitx5/inputmethod/better-wubi.conf ~/.local/share/fcitx5/inputmethod/better-wubi.main.dict
+    rm -f ~/.local/share/fcitx5/inputmethod/better-wubi.{conf,main.dict,png}
 
     fcitx5-remote -e && fcitx5-remote -c
 }
